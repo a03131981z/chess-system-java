f7995287242863d8ddbb48068da845478edaeb53
Lindembergh Madruga <lindembergh@limadruga.localdomain>
2021-07-16 09:34:15 -0300

    Implementando movimentos possíveis do Rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,6 +2,7 @@ package chess.pieces;
 import chess.ChessPiece;
 import chess.Color;
 import board_game.Board;
+import board_game.Position;
 
 public class King extends ChessPiece{
 
@@ -13,10 +14,66 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position posicao) {
+		ChessPiece p = (ChessPiece) getBoard().piece(posicao);
+		return p != null || p.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][]mat = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+		Position p = new Position(0, 0);
+		
+		//Acima
+		p.setValues(posicao.getLinha() -1, posicao.getColuna());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Abaixo
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Esquerda
+		p.setValues(posicao.getLinha(), posicao.getColuna() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Direito
+		p.setValues(posicao.getLinha(), posicao.getColuna() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//noroeste
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//nordeste
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudoeste
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudeste
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
 		return mat;
+		
 	}
 }
